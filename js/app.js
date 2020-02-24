7ff99c55559cf27b3d475bbd21e077b5d35b86a7
ntsmwk <markus.weissenbek@gmx.at>
2020-02-24 04:04:38 +0100

    ..

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@ $('.carousel').carousel({
   interval: 6000,
   pause: 'hover'
 });
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
